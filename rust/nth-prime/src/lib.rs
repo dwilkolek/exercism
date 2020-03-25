e92911d605145727dfd6269ae6c1dd2d7a73dd47
Damian Wilkołek <dwilkolek@protonmail.com>
2020-03-25 23:55:16 +0100

    rust nth prime more elegant

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,22 +1,7 @@
 pub fn nth(n: u32) -> u32 {
-    let mut primes_found = 0;
-    let mut test_number = 1;
-    let end = n + 1;
-    while primes_found != end {
-        test_number += 1;
-        if is_prime(test_number) {
-            primes_found += 1;
-        }
-    }
-
-    test_number
+    (2..).filter(|x| is_prime(*x)).nth(n as usize).unwrap()
 }
 
 fn is_prime(n: u32) -> bool {
-    for div in 2..n - 1 {
-        if n % div == 0 {
-            return false;
-        }
-    }
-    true
+    !(2..n - 1).any(|div| n % div == 0)
 }
