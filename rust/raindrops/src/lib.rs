7c52b68325b14caa082197e627b987e7a1d4c619
Damian Wilkołek <dwilkolek@protonmail.com>
2020-03-25 21:41:55 +0100

    update rust raindrops to use tuples

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,22 +1,12 @@
 pub fn raindrops(n: u32) -> String {
-    if n % 7 != 0 && n % 5 != 0 && n % 3 != 0 {
+    let result: String = vec![(3, "Pling"), (5, "Plang"), (7, "Plong")]
+        .into_iter()
+        .map(|tuple| if n % tuple.0 == 0 { tuple.1 } else { "" })
+        .collect();
+
+    if result.is_empty() {
         n.to_string()
     } else {
-        vec![3, 5, 7]
-            .into_iter()
-            .map(|prime| translate(if n%prime == 0 {prime} else {0}))
-            .fold("".to_string(), |mut acc, translation| {
-                acc.push_str(&*translation);
-                acc
-            })
-    }
-}
-
-fn translate(prime: u32) -> &'static str {
-    match prime {
-        3 => "Pling",
-        5 => "Plang",
-        7 => "Plong",
-        _ => "",
+        result
     }
 }
