a5890f46ea337fadeae0975b2bec4f573b43ecdb
Damian Wilkołek <dwilkolek@protonmail.com>
2020-03-26 22:16:27 +0100

    rust proverb

---
+++ rust/proverb/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn build_proverb(list: &[&str]) -> String {
+    if list.is_empty() {
+        String::from("")
+    } else {
+        _build_proverb(list, &*list[0])
+    }
+}
+
+fn _build_proverb(list: &[&str], first: &str) -> String {
+    match list.len() {
+        1 => format!("And all for the want of a {}.", first),
+        _ => format!(
+            "For want of a {} the {} was lost.\n{}",
+            list[0],
+            list[1],
+            _build_proverb(&list[1..], first)
+        ),
+    }
+}
