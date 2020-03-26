e0a23b3dadf4dba08c7f6314defc9764c969399b
Damian Wilkołek <dwilkolek@protonmail.com>
2020-03-26 19:38:55 +0100

    rust raindrops do not need vec.

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn raindrops(n: u32) -> String {
-    let result: String = vec![(3, "Pling"), (5, "Plang"), (7, "Plong")]
-        .into_iter()
+    let result: String = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
+        .iter()
         .map(|tuple| if n % tuple.0 == 0 { tuple.1 } else { "" })
         .collect();
 
