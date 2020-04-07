21f8518af89b34ddc3eb0fdffc906e57ca076bd2
Damian Wilkołek <dwilkolek@protonmail.com>
2020-04-08 01:05:25 +0200

    make clock a bit cleaner :)

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,48 +1,31 @@
 use std::fmt;
 
-// #[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Clock {
-    state: i32,
+    total_minutes: i32,
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Clock {
-            state: reduce_state(hours * 60 + minutes),
+            total_minutes: (hours * 60 + minutes).rem_euclid(1440),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         Clock {
-            state: reduce_state(self.state + minutes),
+            total_minutes: (self.total_minutes + minutes).rem_euclid(1440),
         }
-    }
-}
-
-impl fmt::Debug for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("").field(&self.state).finish()
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.state / 60, self.state % 60)
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.total_minutes / 60,
+            self.total_minutes % 60
+        )
     }
-}
-
-impl PartialEq for Clock {
-    fn eq(&self, other: &Self) -> bool {
-        self.state == other.state
-    }
-}
-
-fn reduce_state(state: i32) -> i32 {
-    let hours = state / 60 % 24;
-    let minutes = state % 60;
-    let reduced = hours * 60 + minutes;
-    if reduced < 0 {
-        return reduce_state(reduced + 1440);
-    }
-    reduced
 }
